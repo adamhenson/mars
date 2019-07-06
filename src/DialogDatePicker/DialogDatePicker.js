547d39e7e3fb505a53d53390849962880e4b0bac
Adam Henson <adamhenson1979@gmail.com>
2019-07-06 14:52:00 -0400

    calendar

--- src/DialogDatePicker/DialogDatePicker.js
+++ src/DialogDatePicker/DialogDatePicker.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import Button from '@material/react-button';
-import Dialog, {
-  DialogTitle,
-  DialogContent,
-} from '@material/react-dialog';
+import React, { useEffect, useState } from 'react';
+import Dialog, { DialogContent } from '@material/react-dialog';
+import InfiniteCalendar from 'react-infinite-calendar';
+import 'react-infinite-calendar/styles.css';
 import '@material/react-dialog/dist/dialog.css';
 import '@material/react-button/dist/button.css';
 import './DialogDatePicker.css';
 
+const CALENDAR_HEIGHT = 300;
+
 export default ({ isOpen, toggle }) => {
+  const [isCalendarVisible, setIsCalendarVisible] = useState(false);
+
   const onDialogClose = () => {
     // synchronize state
     if (isOpen) {
@@ -16,14 +18,28 @@ export default ({ isOpen, toggle }) => {
     }
   }
 
+  // we use this trick so that `InfiniteCalendar` can calculate where
+  // it needs to be. seems like it has a bug that we're addressing here.
+  useEffect(() => {
+    if (isOpen && !isCalendarVisible) {
+      setIsCalendarVisible(true);
+    }
+  }, [isOpen]);
+
   return (
     <Dialog
       open={isOpen}
       onClose={onDialogClose}
     >
-      <DialogTitle>Filter by Date</DialogTitle>
       <DialogContent>
-        Ipsum lorem
+        {isCalendarVisible &&
+          <InfiniteCalendar
+            displayOptions={{
+              showTodayHelper: false,
+            }}
+            height={CALENDAR_HEIGHT}
+          />
+        }
       </DialogContent>
     </Dialog>
   );
