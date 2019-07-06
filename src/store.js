d58f40079d52a9304ccc63964f4f6d7d2498f6a4
Adam Henson <adamhenson1979@gmail.com>
2019-07-06 18:20:39 -0400

    fix redux extension bug.

--- src/store.js
+++ src/store.js
@@ -2,10 +2,13 @@ import { compose, createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/rootReducer';
 
+const middleware = applyMiddleware(
+	thunk,
+);
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 export default () => createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  ),
+	rootReducer,
+	composeEnhancers(middleware)
 );
